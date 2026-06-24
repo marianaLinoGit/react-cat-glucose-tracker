de56cc02da8a0abb6456c200614416719993e0e0
Mariana Lino <mariana.lino.git@gmail.com>
2026-06-23 22:08:40 -0300

    Deploy github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "export",
+	basePath: "/react-cat-glucose-tracker",
+	assetPrefix: "/react-cat-glucose-tracker/",
 };
 
 export default nextConfig;
